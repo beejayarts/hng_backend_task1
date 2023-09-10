60082603be7e3ad9078eb784fd7248d2eb35220a
Beejay <onuohabenjamin33@gmail.com>
2023-09-10 21:58:10 +0100

    3rd commit

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-
-const slackName = req.query.slack_name || 'example_name';
-const track = req.query.track || 'backend'
-  // json response
+  const {slack_name, track} = req.query
+// const slackName = req.query.slack_name || 'example_name';
+// const track = req.query.track || 'backend'
+//   // json response
   const jsonResponse = {
-    slack_name: "BenjaminOnuoha",
+     slack_name: slack_name,
     current_day: new Date().toISOString('en-US',{timeZone: 'Africa/Lagos',weekday:'long'}),
     utc_time: new Date().toISOString().replace(/\.\d{3}$/,'Z'),
-    track: "backend",
+    track: track,
     github_file_url: "https://github.com/beejayarts/hng_backend_task1/blob/main/index.js",
     github_repo_url: "https://github.com/beejayarts/hng_backend_task1",
     status_code: 200,
